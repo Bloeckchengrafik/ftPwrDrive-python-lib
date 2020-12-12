0c90b7fb1b1027161216796adade6f6ffba6f5fe
Bloeckchengrafik <magiclake@gmx.de>
2020-12-12 15:11:37 +0100

    Uploaded to pypi. Install using pip install ftPwrDrive.py

---
+++ ftpwrdrive/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ftPwrDrive.py',
+    version='1.0',
+    packages=['ftpwrdrive'],
+    url='',
+    license='GPL',
+    author='Bloeckchengrafik',
+    author_email='ftpi@gmx.de',
+    description='The FtPwrDrive Python Lib'
+)
